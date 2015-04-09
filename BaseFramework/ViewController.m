23c3edda2e4c242618106ca85b2c79989d9a184c
kt <kt@a2ndeiMac.local>
2015-04-09 09:21:34 +0800

    Initial Commit

---
+++ BaseFramework/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaseFramework
+//
+//  Created by kt on 15/4/9.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
