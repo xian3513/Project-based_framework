36a8a213531b4c2183124a5924c5585f96c7f7d6
kt <kt@a2ndeiMac.local>
2015-04-29 16:04:01 +0800

    d

--- BaseFramework/AppDelegate.m
+++ BaseFramework/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    ///ddddd
+    ///xis    sss
     NSDictionary * attr = [NSDictionary dictionaryWithObject:[UIColor whiteColor]forKey:NSForegroundColorAttributeName];
     [[UINavigationBar appearance] setTitleTextAttributes:attr];
     [application setStatusBarStyle:UIStatusBarStyleLightContent];
